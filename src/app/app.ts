1df752718e337e464856a3882c1991903f9fde52
Shubham singh <sshubham21072004@gmail.com>
2026-05-18 03:08:47 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  title=signal('my-app')
    
 }
